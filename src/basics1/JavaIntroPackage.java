31fb423dbd147508b44e8851c076014ea65c1572
javaclass <javaclass@15000503CPU.fec.org>
2017-09-26 10:18:28 -0500

    Pets are a billion dollar industry.

---
+++ src/basics1/JavaIntroPackage.java
@@ -0,0 +1,10 @@
+package basics1;
+
+public class JavaIntroPackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("pets are awesome");
+	}
+
+}
