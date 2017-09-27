13f7c1e2e7802290777bde5d8bee6ed96c31efd4
classroom <classroom@15000503CPU.fec.org>
2017-09-27 13:22:04 -0500

    my first program

--- src/basics1/JavaIntroPackage.java
+++ src/basics1/JavaIntroPackage.java
@@ -3,8 +3,6 @@ package basics1;
 public class JavaIntroPackage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("pets are awesome");
+		System.out.println("Hello World");
 	}
-
 }
