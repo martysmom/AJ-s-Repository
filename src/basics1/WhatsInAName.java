58ec47e93301faaec016ef001ca01ac2737275aa
classroom <classroom@15000503CPU.fec.org>
2017-09-27 14:55:24 -0500

    I need to see my name 10x

---
+++ src/basics1/WhatsInAName.java
@@ -0,0 +1,27 @@
+package basics1;
+
+public class WhatsInAName {
+
+	public static void main(String[] args) {
+		  int number = 1;
+
+		  while(number<=10) {
+			  
+			  System.out.println("ARLIETHA");
+			  System.out.println(number);
+			  number = number + 1;
+		  }
+
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
