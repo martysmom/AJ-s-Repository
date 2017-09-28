bf4cad43cff0c22678f9862f890c993f6e202866
classroom <classroom@15000503CPU.fec.org>
2017-09-28 14:09:33 -0500

    Break It

---
+++ src/basics1/BreakIt.java
@@ -0,0 +1,16 @@
+package basics1;
+
+public class BreakIt {
+
+	public static void main(String[] args) {
+
+		for (int i = 10; i >= 1; i--) {
+			if (i == 3 || i==4 || i==6 ) {
+				continue;
+			}
+
+			System.out.println(i);}
+}
+	}
+
+//|| means or||
