5dc2c53e7a83620287017f18f3243d4e2d47cffe
classroom <classroom@15000503CPU.fec.org>
2017-10-04 14:12:42 -0500

    Short string for either true or false

---
+++ src/basics1/StringContains.java
@@ -0,0 +1,13 @@
+package basics1;
+
+public class StringContains {
+
+	public static void main(String[] args) {
+		String message = "Hey, I will be late to class today";
+		String wordIWantToCheck = "late";
+		
+		System.out.println(message.contains(wordIWantToCheck));
+
+	}
+
+}
