be04b13366de280bd30fb4b8e839a683ec2a8beb
classroom <classroom@15000503CPU.fec.org>
2017-10-09 09:54:03 -0500

    I only want the last two.

---
+++ src/basics1/Ly.java
@@ -0,0 +1,27 @@
+package basics1;
+
+public class Ly {
+
+	public static void main(String[] args) {
+		
+System.out.println(endsLY ("sparingly"));
+
+
+	}
+	
+	public static boolean endsLY(String word) {
+	if ((word.length ()<2)) {
+		return false;
+		
+	}
+	
+	if (word.substring(word.length()-2).equals ("Ly")) {
+		
+	}
+	return false;
+	}
+	
+}
+	
+	
+
